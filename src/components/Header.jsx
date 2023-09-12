a5e5d316127ff72edeb3a7062fd92e6f65ebc8fe
Jorge Peraza <jorgeperaza2000@gmail.com>
2023-09-12 21:52:28 +0200

    Nuevo diseño y menus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,50 +1,171 @@
 export default function Header() {
-    return (
-        <>
-            <header className="app-header">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                <ul className="navbar-nav">
-                    <li className="nav-item d-block d-xl-none">
-                    <a className="nav-link sidebartoggler nav-icon-hover" id="headerCollapse">
-                        <i className="ti ti-menu-2"></i>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link nav-icon-hover" href="/">
-                        <i className="ti ti-bell-ringing"></i>
-                        <div className="notification bg-primary rounded-circle"></div>
-                    </a>
-                    </li>
-                </ul>
-                <div className="navbar-collapse justify-content-end px-0" id="navbarNav">
-                    <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link nav-icon-hover" href="/" id="drop2" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <i className="ti ti-user-circle"></i>
-                        </a>
-                        <div className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-                        <div className="message-body">
-                            <a href="/" className="d-flex align-items-center gap-2 dropdown-item">
-                            <i className="ti ti-user fs-6"></i>
-                            <p className="mb-0 fs-3">My Profile</p>
-                            </a>
-                            <a href="/" className="d-flex align-items-center gap-2 dropdown-item">
-                            <i className="ti ti-mail fs-6"></i>
-                            <p className="mb-0 fs-3">My Account</p>
-                            </a>
-                            <a href="/" className="d-flex align-items-center gap-2 dropdown-item">
-                            <i className="ti ti-list-check fs-6"></i>
-                            <p className="mb-0 fs-3">My Task</p>
-                            </a>
-                            <a href="./authentication-login.html" className="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-                        </div>
-                        </div>
-                    </li>
-                    </ul>
-                </div>
-                </nav>
-            </header>
-        </>
-    );
+	return (
+		<>
+			<nav
+				className="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl "
+				id="navbarBlur"
+				data-scroll="false"
+			>
+				<div className="container-fluid py-1 px-3">
+					<nav aria-label="breadcrumb">
+						<ol className="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+							<li className="breadcrumb-item text-sm">
+								<a
+									className="opacity-5 text-white"
+									href="/"
+								>
+									Pages
+								</a>
+							</li>
+							<li
+								className="breadcrumb-item text-sm text-white active"
+								aria-current="page"
+							>
+								Tables
+							</li>
+						</ol>
+						<h6 className="font-weight-bolder text-white mb-0">
+							Tables
+						</h6>
+					</nav>
+					<div
+						className="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4"
+						id="navbar"
+					>
+						<div className="ms-md-auto pe-md-3 d-flex align-items-center">
+						</div>
+						<ul className="navbar-nav  justify-content-end">
+							<li className="nav-item d-flex align-items-center">
+								<a
+									href="/"
+									className="nav-link text-white font-weight-bold px-0"
+								>
+									<i className="fa fa-user me-sm-1"></i>
+									<span className="d-sm-inline d-none">
+										Sign In
+									</span>
+								</a>
+							</li>
+							<li className="nav-item d-xl-none ps-3 d-flex align-items-center">
+								<a
+									href="/"
+									className="nav-link text-white p-0"
+									id="iconNavbarSidenav"
+								>
+									<div className="sidenav-toggler-inner">
+										<i className="sidenav-toggler-line bg-white"></i>
+										<i className="sidenav-toggler-line bg-white"></i>
+										<i className="sidenav-toggler-line bg-white"></i>
+									</div>
+								</a>
+							</li>
+							<li className="nav-item px-3 d-flex align-items-center">
+								<a
+									href="/"
+									className="nav-link text-white p-0"
+								>
+									<i className="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+								</a>
+							</li>
+							<li className="nav-item dropdown pe-2 d-flex align-items-center">
+								<a
+									href="/"
+									className="nav-link text-white p-0"
+									id="dropdownMenuButton"
+									data-bs-toggle="dropdown"
+									aria-expanded="false"
+								>
+									<i className="fa fa-bell cursor-pointer"></i>
+								</a>
+								<ul
+									className="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4"
+									aria-labelledby="dropdownMenuButton"
+								>
+									<li className="mb-2">
+										<a
+											className="dropdown-item border-radius-md"
+											href="/"
+										>
+											<div className="d-flex py-1">
+												<div className="my-auto">
+													<img
+														src="/images/team-2.jpg"
+														className="avatar avatar-sm  me-3"
+													/>
+												</div>
+												<div className="d-flex flex-column justify-content-center">
+													<h6 className="text-sm font-weight-normal mb-1">
+														<span className="font-weight-bold">
+															New message
+														</span>{" "}
+														from Laur
+													</h6>
+													<p className="text-xs text-secondary mb-0">
+														<i className="fa fa-clock me-1"></i>
+														13 minutes ago
+													</p>
+												</div>
+											</div>
+										</a>
+									</li>
+									<li className="mb-2">
+										<a
+											className="dropdown-item border-radius-md"
+											href="/"
+										>
+											<div className="d-flex py-1">
+												<div className="my-auto">
+													<img
+														src="/images/small-logos/logo-spotify.svg"
+														className="avatar avatar-sm bg-gradient-dark me-3"
+													/>
+												</div>
+												<div className="d-flex flex-column justify-content-center">
+													<h6 className="text-sm font-weight-normal mb-1">
+														<span className="font-weight-bold">
+															New album
+														</span>{" "}
+														by Travis Scott
+													</h6>
+													<p className="text-xs text-secondary mb-0">
+														<i className="fa fa-clock me-1"></i>
+														1 day
+													</p>
+												</div>
+											</div>
+										</a>
+									</li>
+									<li>
+										<a
+											className="dropdown-item border-radius-md"
+											href="/"
+										>
+											<div className="d-flex py-1">
+                                            <div className="my-auto">
+													<img
+														src="/images/small-logos/logo-slack.svg"
+														className="avatar avatar-sm bg-gradient-dark me-3"
+													/>
+												</div>
+												<div className="d-flex flex-column justify-content-center">
+													<h6 className="text-sm font-weight-normal mb-1">
+														Payment successfully
+														completed
+													</h6>
+													<p className="text-xs text-secondary mb-0">
+														<i className="fa fa-clock me-1"></i>
+														2 days
+													</p>
+												</div>
+											</div>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
 }
